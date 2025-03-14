c08085c76ad4595d3f83feb065509857a3667989
DavidSenai <silvadavidandrade625@gmail.com>
2025-03-14 15:30:20 -0300

    add banco de dados

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 const express = require("express"); //importou a classe
+const sqlite3 = require("sqlite3");
 
 const port = 8000; // porta TCP do servidor HTTP da aplicação
 
-const app = express(); //cria istancia da classe express
+const app = express(); //Instância para o uso do Express
 
-const index = "<a href='/sobre'> Sobre </a><a href='/info'> Info </a>";
-const sobre = 'vc está na página "Sobre"<br><a href="/">Voltar</a>';
-const info = 'vc está na página "Info"<br><a href="/">Voltar</a>';
+const db = new sqlite3.Database("user.db"); //Instâcia para uso do Sqlite3, e usa o arquivo 'user.db'
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)"
+  );
+});
+
+// Cria conexão com o banco de dados
+
+const Home =
+  "<a href='/sobre'> Sobre </a><a href='/Login'> Login </a><a href='/cadastro'> Cadastro </a>";
+const Sobre = 'vc está na página "Sobre"<br><a href="/">Voltar</a>';
+const Login = 'vc está na página "Login"<br><a href="/">Voltar</a>';
+const Cadastro = 'vc está na página "Cadastro"<br><a href="/">Voltar</a>';
 
 // Metodo express. get necessita de dois parâmetros
 //Na ARROW FUNCTION, O primeiro são dados do servidor (REQUISITION - 'req')
 // o segundo sao os dados que serao enviados ao cliente (result - 'res')
 app.get("/", (req, res) => {
-  res.send(index);
+  res.send(Home);
 });
 
 app.get("/sobre", (req, res) => {
-  res.send(sobre);
+  res.send(Sobre);
 });
 
-app.get("/info", (req, res) => {
-  res.send(info);
+app.get("/login", (req, res) => {
+  res.send(Login);
+});
+
+app.get("/cadastro", (req, res) => {
+  res.send(Cadastro);
 });
 //app.listen() deve ser o último comando da aplicação (app.js)
 app.listen(port, () => {
--- rotas.js
+++ rotas.js
@@ -4,23 +4,28 @@ const port = 8000; // porta TCP do servidor HTTP da aplicação
 
 const app = express(); //cria istancia da classe express
 
-const index = "<a href='/sobre'> Sobre </a><a href='/info'> Info </a>";
+const home = "<a href='/sobre'> Sobre </a><a href='/info'> Info </a>";
 const sobre = 'vc está na página "Sobre"<br><a href="/">Voltar</a>';
-const info = 'vc está na página "Info"<br><a href="/">Voltar</a>';
+const login = 'vc está na página "Login"<br><a href="/">Voltar</a>';
+const cadastro = 'vc está na página "cadastro"<br><a href="/">Voltar</a>';
 
 // Metodo express. get necessita de dois parâmetros
 //Na ARROW FUNCTION, O primeiro são dados do servidor (REQUISITION - 'req')
 // o segundo sao os dados que serao enviados ao cliente (result - 'res')
 app.get("/", (req, res) => {
-  res.send(index);
+  res.send(home);
 });
 
 app.get("/sobre", (req, res) => {
   res.send(sobre);
 });
 
-app.get("/info", (req, res) => {
-  res.send(info);
+app.get("/login", (req, res) => {
+  res.send(login);
+});
+
+app.get("/cadastro", (req, res) => {
+  res.send(cadastro);
 });
 //app.listen() deve ser o último comando da aplicação (app.js)
 app.listen(port, () => {
